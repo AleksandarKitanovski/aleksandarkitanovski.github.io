f657e9b612624887069c5c2177baee37592d88b0
Aleksandar Kitanovski <kitanovski.aleksandar@yahoo.com>
2024-08-22 19:03:18 +0200

    improve about me style
    fix

--- src/routes/about-me/+page.svelte
+++ src/routes/about-me/+page.svelte
@@ -1,8 +1,12 @@
 <script>
 	import Card from './card.svelte';
+	function copyMail() {
+		navigator.clipboard.writeText('kitanovski.aleksandar@yahoo.com');
+		alert('Mail coppied');
+	}
 </script>
 
-<div class="p-3 lg:flex lg:justify-between lg:mt-10">
+<div class="p-3 xl:flex xl:justify-between xl:mt-10">
 	<Card title="Career Summary">
 		I have a bachelors degree in Computer Science from FCSE, and 2 years work experience as a Data
 		Scientist. I love to learn and apply my knowledge, I like working in collaborative environments
@@ -25,16 +29,22 @@
 		You can contact me by E-mail or LinkedIn, also feel free to look around my github:
 		<ul class="flex flex-col p-3 m-3 list-disc">
 			<li class="m-3">
-				LinkedIn: <a href="/" class="text-orange-600 hover:text-orange-400">Aleksandar Kitanovski</a
+				LinkedIn: <a
+					href="https://www.linkedin.com/in/aleksandar-kitanovski/"
+					class="text-orange-600 hover:text-orange-400">Aleksandar Kitanovski</a
 				>
 			</li>
 			<li class="m-3">
-				Github: <a href="/" class="text-orange-600 hover:text-orange-400">AleksandarKitanovski</a>
+				Github: <a
+					href="https://github.com/AleksandarKitanovski"
+					class="text-orange-600 hover:text-orange-400">AleksandarKitanovski</a
+				>
 			</li>
 			<li class="m-3">
-				E-mail: <span class="text-orange-600 hover:text-orange-400"
-					>kitanovski.aleksandar@yahoo.com</span
-				>
+				E-mail:
+				<button class="text-orange-600 hover:text-orange-400" on:click={copyMail}>
+					kitanovski.aleksandar@yahoo.com
+				</button>
 			</li>
 		</ul>
 	</Card>
