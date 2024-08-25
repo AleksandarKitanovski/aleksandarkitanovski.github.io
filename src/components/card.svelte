9f8d94b48dae1092c0eb37338c6e27f9ce50f1a7
Aleksandar Kitanovski <kitanovski.aleksandar@yahoo.com>
2024-08-25 11:42:15 +0200

    refactor: make a compoments folder and move card.svelte there

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -2,7 +2,7 @@
 	export let title = '';
 </script>
 
-<div class="border-2 border-gray-800 rounded-md w-full my-4 mx-auto p-4 xl:w-1/3 xl:m-4">
+<div class="border-2 border-gray-800 rounded-md w-full my-4 mx-auto p-4 xl:m-4">
 	<h1 class="text-center mb-3 text-2xl text-orange-600">{title}</h1>
 	<p class="text-justify">
 		<slot />
--- src/routes/about-me/+page.svelte
+++ src/routes/about-me/+page.svelte
@@ -1,12 +1,12 @@
 <script>
-	import Card from './card.svelte';
+	import Card from '../../components/card.svelte';
 	function copyMail() {
 		navigator.clipboard.writeText('kitanovski.aleksandar@yahoo.com');
 		alert('Mail coppied');
 	}
 </script>
 
-<div class="p-3 xl:flex xl:justify-between xl:mt-10">
+<div class="m-3 xl:flex xl:justify-between xl:mt-10">
 	<Card title="Career Summary">
 		I have a bachelors degree in Computer Science from FCSE, and 2 years work experience as a Data
 		Scientist. I love to learn and apply my knowledge, I like working in collaborative environments
