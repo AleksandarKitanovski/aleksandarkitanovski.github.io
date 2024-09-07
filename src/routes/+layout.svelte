1c2a58728c6e4b5cbfddb691b730353c8d3113f3
Aleksandar Kitanovski <kitanovski.aleksandar@yahoo.com>
2024-09-07 12:15:05 +0200

    ui updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,22 +18,47 @@
 		<nav class="bg-gray-800 text-white h-auto w-full sticky top-0 left-0 z-50">
 			<ul class="md:flex relative">
 				<div class="flex mr-auto">
-					<a href="/" class="mr-auto"
-						><li class="p-3 hover:bg-gray-700">Aleksandar Kitanovski</li></a
-					>
+					<a href="/" class="mr-auto" on:click={close_menu}>
+						<li class="p-3 hover:bg-gray-700">Aleksandar Kitanovski</li>
+					</a>
 					<button type="button" class="md:hidden p-2 hover:bg-gray-700" on:click={toggle_menu}>
-						<svg
-							width="32px"
-							height="30px"
-							viewBox="0 0 24 24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-							stroke="white"
-						>
-							<path d="M4 18L20 18" stroke-width="2" stroke-linecap="round" />
-							<path d="M4 12L20 12" stroke-width="2" stroke-linecap="round" />
-							<path d="M4 6L20 6" stroke-width="2" stroke-linecap="round" />
-						</svg>
+						{#if hidden}
+							<svg
+								width="32px"
+								height="30px"
+								viewBox="0 0 24 24"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								stroke="white"
+							>
+								<path d="M4 18L20 18" stroke-width="2" stroke-linecap="round" />
+								<path d="M4 12L20 12" stroke-width="2" stroke-linecap="round" />
+								<path d="M4 6L20 6" stroke-width="2" stroke-linecap="round" />
+							</svg>
+						{:else}
+							<svg
+								width="32px"
+								height="22px"
+								viewBox="-0.5 0 25 25"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M3 21.32L21 3.32001"
+									stroke="white"
+									stroke-width="3"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+								<path
+									d="M3 3.32001L21 21.32"
+									stroke="white"
+									stroke-width="3"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+							</svg>
+						{/if}
 					</button>
 				</div>
 				<div
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,29 @@
-<h1>Welcome to your new page!</h1>
+<script>
+	import List from '../components/list.svelte';
+	import ListItem from '../components/list_item.svelte';
+</script>
+
+<List>
+	<ListItem title="Sep, 1999 - Landed on Earth">
+		Right before Krypton was destroyed, my parents loaded me up in an escape pod and sent me to
+		Earth, along with my cousin Kal-El (you might know him as Clark Kent). While Clark decided to be
+		Superman, I decided to be a super programmer.
+	</ListItem>
+	<ListItem title="Sep, 2005 - Started Elementary School">
+		Playing Super Mario among other games on a NES was my first introduction to gaming, and what
+		originally drew me towards programming as a career. This was also around the time I got my first
+		computer, and with more games came more curiosity - why not learn how to make them myself one
+		day?
+	</ListItem>
+	<ListItem title="Sep, 2010 - Started Middle School">
+		During this time I got access to the internet, and started figuring out how to write batch
+		scripts on Windows XP, after a while I discovered C++ and started teaching myself programming. I
+		was having more fun than I could imagine.
+	</ListItem>
+	<ListItem title="Sep, 2014 - Started High School"></ListItem>
+	<ListItem title="Oct, 2018 - Started College"></ListItem>
+	<ListItem title="Jul, 2021 - Started an ML Internship"></ListItem>
+	<ListItem title="Mar, 2022 - Started Working at GrabIT"></ListItem>
+	<ListItem title="Apr, 2023 - Graduated from college"></ListItem>
+	<ListItem title="May, 2024 - Started Working at Ludotech"></ListItem>
+</List>
