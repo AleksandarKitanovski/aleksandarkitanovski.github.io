fd84d0c6437ea41c8ca0a62cf4bef9f9e54dd613
Aleksandar Kitanovski <kitanovski.aleksandar@yahoo.com>
2024-09-16 18:50:17 +0200

    replace lolipop chart with carousel slides

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,104 +1,70 @@
 <script lang="ts">
+	import Carousel from '../components/carousel.svelte';
 	import List from '../components/list.svelte';
 	import ListItem from '../components/list_item.svelte';
-	import Lolipop from '../components/lolipop.svelte';
-	import LolipopBottom from '../components/lolipop_bottom.svelte';
-	import LolipopChart from '../components/lolipop_chart.svelte';
-	import LolipopSpacer from '../components/lolipop_spacer.svelte';
 </script>
 
-<div class="md:hidden">
+<div class="hidden">
 	<List>
-		<ListItem title="Sep, 1999 - Landed on Earth">
-			Right before Krypton was destroyed, my parents loaded me up in an escape pod and sent me to
-			Earth, along with my cousin Kal-El (you might know him as Clark Kent). While Clark decided to
-			be Superman, I decided to be a super programmer.
-		</ListItem>
-		<ListItem title="Sep, 2005 - Started Elementary School">
-			Playing Super Mario among other games on a NES was my first introduction to gaming, and what
-			originally drew me towards programming as a career. This was also around the time I got my
-			first computer, and with more games came more curiosity - why not learn how to make them
-			myself one day?
-		</ListItem>
-		<ListItem title="Sep, 2010 - Started Middle School">
-			During this time I got access to the internet, and started figuring out how to write batch
-			scripts on Windows XP, after a while I discovered C++ and started teaching myself programming.
-			I was having more fun than I could imagine.
-		</ListItem>
-		<ListItem title="Sep, 2014 - Started High School"></ListItem>
-		<ListItem title="Oct, 2018 - Started College"></ListItem>
 		<ListItem title="Jul, 2021 - Started an Internship at Nebb"></ListItem>
 		<ListItem title="Mar, 2022 - Started Working at GrabIT"></ListItem>
-		<ListItem title="Apr, 2023 - Graduated from college"></ListItem>
 		<ListItem title="May, 2024 - Started Working at Ludotech"></ListItem>
 	</List>
 </div>
-<LolipopChart>
-	<div slot="top" class="flex justify-between mt-10 px-10">
-		<Lolipop
-			title="Landed on Earth"
-			date="Sep, 1999"
-			border_color="border-red-600"
-			bg_color="bg-red-600"
-		/>
-		<LolipopSpacer />
-		<Lolipop
-			title="Started Middle School"
-			date="Sep, 2010"
-			border_color="border-red-300"
-			bg_color="bg-red-300"
-		/>
-		<LolipopSpacer />
-		<Lolipop
-			title="Started College"
-			date="Sep, 2018"
-			border_color="border-yellow-500"
-			bg_color="bg-yellow-500"
-		/>
-		<LolipopSpacer />
-		<Lolipop
-			title="Started Working at GrabIT"
-			date="Mar, 2022"
-			border_color="border-blue-400"
-			bg_color="bg-blue-400"
-		/>
-		<LolipopSpacer />
-		<Lolipop
-			title="Started Working at Ludotech"
-			date="May, 2024"
-			border_color="border-blue-800"
-			bg_color="bg-blue-800"
-		/>
-	</div>
-	<div slot="bottom" class="flex justify-between mb-10 px-10">
-		<LolipopSpacer />
-		<LolipopBottom
-			title="Started Elementary School"
-			date="Sep, 2005"
-			border_color="border-red-400"
-			bg_color="bg-red-400"
-		/>
-		<LolipopSpacer />
-		<LolipopBottom
-			title="Started High School"
-			date="Sep, 2014"
-			border_color="border-yellow-300"
-			bg_color="bg-yellow-300"
-		/>
-		<LolipopSpacer />
-		<LolipopBottom
-			title="Started an Internship at Nebb"
-			date="Jul, 2021"
-			border_color="border-yellow-300"
-			bg_color="bg-yellow-300"
-		/>
-		<LolipopSpacer />
-		<LolipopBottom
-			title="Graduated from college"
-			date="Apr, 2023"
-			border_color="border-blue-600"
-			bg_color="bg-blue-600"
-		/>
-		<LolipopSpacer />
-	</div>
-</LolipopChart>
+<Carousel
+	slides={[
+		{
+			title: 'Landed on Earth',
+			text: `Right before my home planet was destroyed,
+			my parents put me in an escape pod and sent me to Earth.
+			I'm not a super hero tho, that's Kal-El, he was my neighbour
+			on Krypton - and like every good neighbour's kid he always has
+			to one up me...`,
+			date: 'September, 1999',
+			img: '/space.jpg'
+		},
+		{
+			title: 'Elementary School',
+			text: `Playing Super Mario among other games on a NES was my first introduction to gaming, and what
+    		originally drew me towards programming as a career. This was also around the time I got my
+    		first computer, and with more games came more curiosity - why not learn how to make them
+    		myself one day?`,
+			date: 'September, 2005 - June, 2010',
+			img: '/elementary.jpg'
+		},
+		{
+			title: 'Middle School',
+			text: `During this time I got access to the internet, and started figuring out how to write batch
+			scripts on Windows XP, after a while I discovered C++ and started teaching myself programming.
+			I was having more fun than I could imagine.`,
+			date: 'September, 2010 - June, 2014',
+			img: '/middle.jpg'
+		},
+		{
+			title: 'High School',
+			text: `As I was getting more serious about programming, I found out I'll need to learn math. The
+			more math I learned the more I wanted to learn, I loved it. I also went on a few programming competitions
+			and found out how much more there was to learn about algorithms, data structures and whatnot...`,
+			date: 'September, 2014 - June, 2018',
+			img: '/high.jpg'
+		},
+		{
+			title: 'Internship at Nebb',
+			text: ``,
+			date: 'July, 2021 - October , 2023',
+			img: '/college.jpg'
+		},
+		{
+			title: 'Joined GrabIT',
+			text: ``,
+			date: 'March, 2022 - April, 2024',
+			img: '/grabit.png'
+		},
+		{
+			title: 'Joined Ludotech',
+			text: ``,
+			date: 'May, 2024 - Present',
+			img: '/college.jpg'
+		}
+	]}
+/>
