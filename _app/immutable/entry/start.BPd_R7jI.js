bc9b74956ecf1bd9256705f5afa5d5a735cecb6f
Aleksandar Kitanovski <kitanovski.aleksandar@yahoo.com>
2024-09-21 11:13:16 +0200

    Updates

---
+++ _app/immutable/entry/start.BPd_R7jI.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CY6R-AMR.js";export{t as start};
---
+++ _app/immutable/nodes/3.Dva7yHFb.js
@@ -0,0 +1,14 @@
+import{s as o,n as a}from"../chunks/scheduler.-dhH77K1.js";import{S as l,i as r,e as m,c as d,g as c,a as g,b as h,d as p}from"../chunks/index.BYsXf19S.js";function v(s){let e,n=`<article><h1 class="text-4xl lg:text-5xl text-orange-600 mb-2">Career Summary</h1> <p class="text-justify text-lg lg:text-xl">I have a bachelors degree in Computer Science from FINKI, and 2 years work experience as a
+			Data Scientist. I love to learn and apply my knowledge, I like working in collaborative
+			environments and I am a great team player. The focus of my studies was mostly Data Science and
+			Applied Mathematics, while my work experience includes web development at Ludotech, chat-bot
+			development and data analysis at GrabIT, and developing systems for time series anomaly
+			detection at Nebb (now called Init). During my studies at FCSE I also published three research
+			papers, two of them at the CiiT Conference in Macedonia, and one at the MIPRO Convention in
+			Croatia.</p></article> <article class="mt-10"><h1 class="text-4xl lg:text-5xl text-orange-600 mb-2">Hobbies</h1> <p class="text-justify text-lg lg:text-xl">I have several hobbies that keep life exciting and fulfilling. I&#39;m an avid gamer, immersing
+			myself in virtual worlds and challenging adventures. I also love reading, which offers an
+			escape into captivating narratives and thought-provoking ideas. Chess is something I&#39;ve loved
+			to play ever since I was a child, I&#39;m not very good at it, but the game is very dear to me.
+			Programming is both a profession and a passion, ever since I wrote my first &quot;Hello, world!&quot;
+			more than 10 years ago. And my most favorite things to do are cooking and baking - I love
+			trying out new recipes and making food for myself and my loved ones.</p></article>`;return{c(){e=m("div"),e.innerHTML=n,this.h()},l(t){e=d(t,"DIV",{class:!0,"data-svelte-h":!0}),c(e)!=="svelte-1yavqpw"&&(e.innerHTML=n),this.h()},h(){g(e,"class","mt-2 mb-16 lg:my-10 mx-4 md:mx-32 lg:mx-80")},m(t,i){h(t,e,i)},p:a,i:a,o:a,d(t){t&&p(e)}}}class u extends l{constructor(e){super(),r(this,e,null,v,o,{})}}export{u as component};
