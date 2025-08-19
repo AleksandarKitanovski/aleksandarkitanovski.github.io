3ab9212a1fa5c5a94d9563dd218fe04683bf53c9
Aleksandar Kitanovski <kitanovskialeksandar@yahoo.com>
2025-08-19 22:53:45 +0200

    Updates

---
+++ _app/immutable/nodes/3.Bwwy0Bty.js
@@ -0,0 +1,15 @@
+import"../chunks/CWj6FrbW.js";import"../chunks/9MT4eJaW.js";import{n as a,o as n}from"../chunks/DTmp4FBA.js";var i=a(`<div class="mt-2 mb-16 lg:my-10 mx-4 md:mx-32 lg:mx-80"><article><h1 class="text-4xl lg:text-5xl text-orange-600 mb-2">Career Summary</h1> <p class="text-justify text-lg lg:text-xl">I hold a bachelor’s degree in Computer Science from FINKI, where I focused on Data Science and
+			Applied Mathematics, and I am currently pursuing a master’s degree in Intelligent Systems at
+			the same faculty. I have 2 years of professional experience as a Data Scientist, and I
+			currently work as a Software Engineer at Ludotech. My experience includes Web Development and
+			AI Engineering at Ludotech, chatbot development and data analysis at GrabIT, and building time
+			series anomaly detection systems at Nebb (now Init). I enjoy learning, applying my knowledge,
+			and working in collaborative environments where I can contribute as a strong team player.
+			During my studies at FINKI, I also published three research papers—two at the CiiT Conference
+			in Macedonia and one at the MIPRO Convention in Croatia.</p></article> <article class="mt-10"><h1 class="text-4xl lg:text-5xl text-orange-600 mb-2">Hobbies</h1> <p class="text-justify text-lg lg:text-xl">I have a bunch of hobbies that keep life fun and interesting. I’m a gamer at heart, always
+			diving into immersive worlds and challenging adventures. I also love reading—it’s my way to
+			escape into captivating stories and explore thought-provoking ideas. Chess has been with me
+			since I was a kid; I’m far from an expert, but the game has always meant a lot to me.
+			Programming isn’t just my job, it’s something I’ve loved ever since I wrote my first “Hello,
+			world!” over ten years ago. And my absolute favorite thing? Cooking and baking. I love trying
+			out new recipes and sharing the results with friends and family.</p></article></div>`);function l(e){var t=i();n(e,t)}export{l as component};
